a8b2c7668e2b33074e4575e198d618c15b599b79
Jeff Fessler <fessler@umich.edu>
2021-05-12 07:58:19 -0400

    test load/save

--- test/file-io.jl
+++ test/file-io.jl
@@ -5,28 +5,20 @@ test AVSfldIO through FileIO
 
 using FileIO: File, @format_str
 using FileIO: add_format, load, save, UUID
-using FileIO: info, unknown, query
-using AVSfldIO
+#using FileIO: info, unknown, query
+#using AVSfldIO
 
-#data1 = rand(Int32, 4, 5)
-data1 = reshape(1:20, 4, 5)
+data1 = reshape(Int32.(1:20), 4, 5)
+file = tempname() * ".fld"
 
-file = "tmp.fld"
+#ff = File{format"FLD"}(file)
+#query(ff)
+
+add_format(format"FLD", "", ".fld",
+    [:AVSfldIO => UUID("b6189060-daf9-4c28-845a-cc0984b81781")])
+
+save(file, data1)
+data2 = load(file)
+@assert data2 == data1
+
 rm(file, force=true)
-
-ff = File{format"FLD"}(file)
-query(ff)
-
-if false # test the methods
-	AVSfldIO.save(ff, data1)
-	data2 = AVSfldIO.load(ff)
-	@assert data2 == data1
-end
-
-add_format(format"FLD", "", ".fld", [:AVSfldIO => UUID("b6189060-daf9-4c28-845a-cc0984b81781")])
-
-if true # test FileIO
-	save(file, data1)
-	data2 = load(file)
-	@assert data2 == data1
-end
