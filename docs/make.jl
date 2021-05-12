3cb640cca160d90828c3f8cf057500e805421742
Jeff Fessler <fessler@umich.edu>
2021-05-11 22:59:43 -0400

    docs

---
+++ docs/make.jl
@@ -0,0 +1,29 @@
+# push!(LOAD_PATH,"../src/")
+
+using AVSfldIO
+using Documenter
+
+DocMeta.setdocmeta!(AVSfldIO, :DocTestSetup, :(using AVSfldIO); recursive=true)
+
+makedocs(;
+    modules = [AVSfldIO],
+    authors = "Jeff Fessler <fessler@umich.edu> and contributors",
+    repo = "https://github.com/JeffFessler/AVSfldIO.jl/blob/{commit}{path}#{line}",
+    sitename = "AVSfldIO.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+#       canonical = "https://JeffFessler.github.io/AVSfldIO.jl/stable",
+#       assets = String[],
+    ),
+    pages = [
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo = "github.com/JeffFessler/AVSfldIO.jl.git",
+    devbranch = "main",
+    devurl = "dev",
+    versions = ["stable" => "v^", "dev" => "dev"]
+#   push_preview = true,
+)
---
+++ docs/src/index.md
@@ -0,0 +1,27 @@
+```@meta
+CurrentModule = AVSfldIO
+```
+
+# AVSfldIO.jl Documentation
+
+## Contents
+
+```@contents
+```
+
+## Overview
+
+File I/O routines for AVS `.fld` format data files;
+see
+[(AVSfldIO)](https://github.com/JeffFessler/AVSfldIO.jl)
+
+## Index
+
+```@index
+```
+
+## Functions
+
+```@autodocs
+Modules = [AVSfldIO]
+```
