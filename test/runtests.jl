dcb14e035d6f989a2e5a95756cd99c8010092ea2
Jeff Fessler <fessler@umich.edu>
2021-05-11 22:49:08 -0400

    test

---
+++ test/runtests.jl
@@ -0,0 +1,17 @@
+# test/runtests.jl
+
+using AVSfldIO
+using Test: @test, @testset, detect_ambiguities
+
+list = [
+"fld-read.jl"
+"fld-write.jl"
+]
+
+for file in list
+    @testset "$file" begin
+        include(file)
+    end
+end
+
+@test length(detect_ambiguities(AVSfldIO)) == 0
