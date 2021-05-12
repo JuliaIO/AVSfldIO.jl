179694699b415143386c3f4103092323e84ef107
Jeff Fessler <fessler@umich.edu>
2021-05-11 22:32:12 -0400

    src

--- src/AVSfldIO.jl
+++ src/AVSfldIO.jl
@@ -1,4 +1,16 @@
+"""
+    AVSfldIO
+module for AVS .fld file IO
+"""
 module AVSfldIO
 
+using FileIO
+
+include("fld-read.jl")
+include("fld-write.jl")
+
+# the two key methods:
+load = fld_read
+save = fld_write
 
 end # module
